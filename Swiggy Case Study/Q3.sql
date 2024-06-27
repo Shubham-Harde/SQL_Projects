e2f460f432ffb8852b358d94f9a468d2a99d6f24
Shubham-Harde <shubham.harde@yahoo.com>
2024-06-27 16:28:14 +0530

    Delete Swiggy Case Study/Q3.sql

--- Swiggy Case Study/Q3.sql
+++
@@ -1,9 +0,0 @@
--- Find top restaurant in terms of number of orders for a given month
-
-select r.r_name, count(*) as 'month' 
-from orders o
-join restaurants r
-on o.r_id = r.r_id
-where monthname(date) like 'June'
-group by r.r_name
-order by count(*) desc limit 1;
