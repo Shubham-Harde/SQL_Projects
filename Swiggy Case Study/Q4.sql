266aab4deaa7dba93be4880f9469ed03e9275f9f
Shubham-Harde <shubham.harde@yahoo.com>
2024-06-27 16:28:24 +0530

    Delete Swiggy Case Study/Q4.sql

--- Swiggy Case Study/Q4.sql
+++
@@ -1,9 +0,0 @@
--- Restaurants with monthly sales>x for
-
-select r.r_name, sum(amount) as 'revenue'
-from  orders o
-join restaurants r
-on o.r_id = r.r_id
-where monthname(date) like 'June'
-group by r.r_name
-having revenue > 500;
